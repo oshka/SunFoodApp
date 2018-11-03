f8a26f45719cd876914d79e5a2416e1c42203f22
oshka <olkace@gmail.com>
2018-11-03 22:13:04 +0200

    Header with logo

--- src/components/header.js
+++ src/components/header.js
@@ -1,18 +1,19 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import logo from '../images/logo.png'
 
 const Header = ({ siteTitle }) => (
   <div
     style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
+      background: '#A60000',
+      //marginBottom: '1.45rem',
     }}
   >
     <div
       style={{
         margin: '0 auto',
         maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
+   //     padding: '1.45rem 1.0875rem',
       }}
     >
       <h1 style={{ margin: 0 }}>
@@ -23,7 +24,7 @@ const Header = ({ siteTitle }) => (
             textDecoration: 'none',
           }}
         >
-          {siteTitle}
+          <img src={logo} alt="Logo" alt="{siteTitle}" title="{siteTitle}"/>
         </Link>
       </h1>
     </div>
