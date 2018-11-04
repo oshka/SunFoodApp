e3a3c162709c7e85454c23c0ac537bdafe62e95e
Alberto Escala <albertoaesc@gmail.com>
2018-11-04 06:56:48 -0500

    fix indent issues

--- src/components/header.js
+++ src/components/header.js
@@ -3,52 +3,51 @@ import { Link } from 'gatsby'
 import logo from '../images/logo02.png'
 
 const Header = ({ siteTitle }) => (
-  <div><div
-    style={{
-      background: '#ea5455',
-    }}
-  >
+  <div>
     <div
       style={{
-        margin: '0 auto',
-        maxWidth: 960,
+        background: '#ea5455',
       }}
     >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          <img src={logo} alt="Logo" alt="{siteTitle}" title="{siteTitle}" className="logo-img"/>
-        </Link>
-      </h1>
+      <div
+        style={{
+          margin: '0 auto',
+          maxWidth: 960,
+        }}
+      >
+        <h1 style={{ margin: 0 }}>
+          <Link
+            to="/"
+            style={{
+              color: 'white',
+              textDecoration: 'none',
+            }}
+          >
+            <img src={logo} alt="Logo" title={siteTitle} className="logo-img" />
+          </Link>
+        </h1>
+      </div>
+    </div>
+    <div
+      style={{
+        background: '#2d4059',
+      }}
+    >
+      <div
+        style={{
+          margin: '0 auto',
+          maxWidth: 960,
+        }}
+      >
+        <ul className="top-menu">
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/">Search Food</Link></li>
+          <li><Link to="/gallery">View Food Collections</Link></li>
+          <li><Link to="/">Contact Us</Link></li>
+        </ul>
+      </div>
     </div>
   </div>
-  <div 
-style={{
-  background: '#2d4059',
-  }}>
-<div
-  style={{
-    margin: '0 auto',
-    maxWidth: 960,
-  }}
->
-<ul className="top-menu">
-  <li><Link to="/">Home</Link></li>
-  <li><a href="">Search Food</a></li>
-  <li><Link to="/gallery">View Food Collections</Link></li>
-  <li><a href="">Contact Us</a></li>
-
-</ul>
- 
-</div>
-</div>
-  </div>
-
 )
 
 export default Header
