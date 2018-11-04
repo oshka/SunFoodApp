018ddc7999f4f39f847af410132ba85d8e23fe65
oshka <olkace@gmail.com>
2018-11-04 10:28:59 +0200

    First Page is ready. Need right links

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import styled from 'styled-components'
+import food from '../images/food.png'
 
 import Layout from '../components/layout'
 import Image from '../components/image'
@@ -20,11 +21,10 @@ const IndexPage = () => (
     <p>We present you a mega application using the latest technologies that can solve all the above problems.</p>
     <p>In our great app we use only morder API such as Fauna (for call the db and receive data), Clarify (to detect food image), Formspree (for collecting delivery address), Pilon (because it's interesting to try). And many, many others.</p>
     <p>Meet ...</p>
-    <h1><a href=""><span>Sun</span>Food<span>App</span></a></h1>
-    <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
-      <Image />
+    <h1 style={{ textAlign:'center', }}><a href=""><span>Sun</span>Food<span>App</span></a></h1>
+    <div style={{ maxWidth: '300px', margin: 'auto', textAlign:'center', }}>
+    <img src={food} alt="Logo" alt="{siteTitle}" title="{siteTitle}"/>
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
 
